6bbba355f96bed3973129476623213e19351ec26
Denis <corvus007@yandex.ru>
2017-12-05 13:02:08 +0300

    Доработки по сайту

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
   svg4everybody();
-  $.fancybox.defaults.animationEffect = "zoom-in-out";
+  $.fancybox.defaults.animationEffect = 'zoom-in-out';
+
+  var clientReviewsSlider = document.querySelector('.client-reviews-slider');
+
+  if (clientReviewsSlider && window.matchMedia('(max-width: 615px)').matches) {
+    $(clientReviewsSlider).slick({
+      accessibility: false,
+      dots: true,
+      arrows: false,
+      autoplay: true
+    });
+  }
+
+  $('input[type="tel"]').mask('+7 (999) 999-99-99', {});
 });
