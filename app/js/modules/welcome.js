91594b8208e7446686a6a85c3b500e408cc4bc16
Денис <corvus007@yandex.ru>
2017-12-19 22:25:05 +0300

    Добавляет дополнительное условие в файле welcome.js

--- app/js/modules/welcome.js
+++ app/js/modules/welcome.js
@@ -3,7 +3,7 @@ window.welcome = (function() {
 
   var welcome = document.querySelector('.welcome');
 
-  if (welcome) {
+  if (welcome && window.matchMedia('(min-height: 568px)').matches) {
     welcome.style.height =
       'calc(100% - ' + window.mainHeader.wrapper.offsetHeight + 'px)';
   }
