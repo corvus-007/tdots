1e34dbc5af038d046770c5eb7a735c016df93f9a
Денис <corvus007@yandex.ru>
2017-12-19 22:27:13 +0300

    Оборачивает код в IIFE и импортиует jQuery в файле script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-  svg4everybody();
-  $.fancybox.defaults.animationEffect = 'zoom-in-out';
+(function ($) {
+  document.addEventListener('DOMContentLoaded', function () {
+    svg4everybody();
+    $.fancybox.defaults.animationEffect = 'zoom-in-out';
 
-  var clientReviewsSlider = document.querySelector('.client-reviews-slider');
+    var clientReviewsSlider = document.querySelector('.client-reviews-slider');
 
-  if (clientReviewsSlider && window.matchMedia('(max-width: 615px)').matches) {
-    $(clientReviewsSlider).slick({
-      accessibility: false,
-      dots: true,
-      arrows: false
-    });
-  }
+    if (clientReviewsSlider && window.matchMedia('(max-width: 615px)').matches) {
+      $(clientReviewsSlider).slick({
+        accessibility: false,
+        dots: true,
+        arrows: false
+      });
+    }
 
-  $('input[type="tel"]').mask('+7 (999) 999-99-99', {});
-});
+    $('input[type="tel"]').mask('+7 (999) 999-99-99', {});
+  });
+})(jQuery);
